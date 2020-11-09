fb15e69ceb7c5ffdfc345ab3692734251d2ebe04
Dominic Marcotte <dominic.marcotte2@usherbrooke.ca>
2020-11-09 14:56:02 -0500

    Cleanning

--- lotb-compendium/extract-characters.js
+++ lotb-compendium/extract-characters.js
@@ -1,12 +1,6 @@
 const puppeteer = require('puppeteer')
 const extractCharacter = require('./extract-character').run
 
-const f = () => {
-  return new Promise(async (resolve, reject) => {
-    setTimeout(() => { resolve() }, 1000)
-  })
-}
-
 async function run () {
   const browser = await puppeteer.launch({
     // devtools: true,
@@ -41,23 +35,5 @@ function generateId (href) {
   const splitted = href.split('/')
   return (splitted[splitted.length - 1] === '') ? splitted[splitted.length - 2] : splitted[splitted.length - 1]
 }
-
-// async function postData (url = '', data = {}) {
-//   // Default options are marked with *
-//   const response = await fetch(url, {
-//     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-//     mode: 'cors', // no-cors, *cors, same-origin
-//     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-//     credentials: 'same-origin', // include, *same-origin, omit
-//     headers: {
-//       'Content-Type': 'application/json'
-//       // 'Content-Type': 'application/x-www-form-urlencoded',
-//     },
-//     redirect: 'follow', // manual, *follow, error
-//     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-//     body: JSON.stringify(data) // body data type must match "Content-Type" header
-//   })
-//   return response.json() // parses JSON response into native JavaScript objects
-// }
 
 run()
